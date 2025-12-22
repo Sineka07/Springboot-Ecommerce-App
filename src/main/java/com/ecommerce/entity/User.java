8e240b9ad6512680622915b04e5359083ec233de
Sineka07 <snehasenthilkumar91@gmail.com>
2025-12-22 21:03:11 +0530

    Create SecuritConfig.java

--- src/main/java/com/ecommerce/entity/User.java
+++ src/main/java/com/ecommerce/entity/User.java
@@ -1,12 +1,13 @@
 
 package com.ecommerce.entity;
 
-import java.lang.annotation.Inherited;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.annotation.processing.Generated;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -22,7 +23,7 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)     
     private Long id;
@@ -76,6 +77,31 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
     
      
@@ -156,4 +182,5 @@ public class User {
         USER,
         ADMIN
     }   
+    
 }
